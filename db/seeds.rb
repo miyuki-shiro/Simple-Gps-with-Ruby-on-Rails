c361b64129e8ba4a9d301b562373ccbf8a9920c3
miyuki_shiro <thaimyg@yahoo.cl>
2019-06-29 03:23:54 -0400

    Some changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Vehicle.create([{ vehicle_name: 'AB-1234' }, { vehicle_name: 'CD-5678' }, { vehicle_name: 'EF-9101' }])
+Waypoint.create([{ latitude: '-33.444592', longitude: '-70.656028', sent_at: '2019-06-24 01:11:00 UTC', vehicle_id: 1}, 
+{ latitude: '-33.408911', longitude: '-70.568381', sent_at: '2019-06-25 20:04:00 UTC', vehicle_id: 2}, 
+{ latitude: '-33.419978', longitude: '-70.612986', sent_at: '2019-06-28 23:30:00 UTC', vehicle_id: 3}])
